3f165c9ade7e619c77ae8a2e057fe286befcc1fe
Helverton Santos <helvertonsantos@gmail.com>
2022-04-08 13:50:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ async function getMlb(){
               '--ignore-certifcate-errors-spki-list',
               '--user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"'
             ],
-      executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+      executablePath: "PATH\\chrome.exe",
       headless: false
     }
     const browser = await puppeteer.launch(options)
